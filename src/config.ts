2126d78e04504d62b1604c4297493e2d0fe76166
jowi <jowiaoun@hotmail.com>
2024-11-08 20:39:39 -0500

    chore(config): add log explicit flag

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+export const LOG_READY_EXPLICIT = false
+
 export const BOT_PREFIX: string[] = process.env.BOT_PREFIX ? process.env.BOT_PREFIX.split(',') : [];
 export const BOT_TOKEN: string = process.env.BOT_TOKEN || '';
 export const OWNER_IDS: string[] = process.env.OWNER_IDS ? process.env.OWNER_IDS.split(',') : [];
